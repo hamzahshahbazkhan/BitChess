e9ba2819ad0575eaa146550de7d66a89571d12e7
dread_pirate_roberts <khanhamzah142@gmail.com>
2024-07-28 20:27:31 +0530

    process.env ;

--- backend1/index.js
+++ backend1/index.js
@@ -224,7 +224,7 @@ io.on("connection", (socket) => {
     })
 });
 
-httpServer.listen(3000, '0.0.0.0', () => {
+httpServer.listen(process.env.PORT, '0.0.0.0', () => {
     //console.log("listening on 3000");
 });
 
